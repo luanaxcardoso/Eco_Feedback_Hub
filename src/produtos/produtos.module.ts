42fd609e941b383e214471a868af8aefdc3e73de
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-10-11 23:24:27 -0300

    feat: criação de produtos

---
+++ src/produtos/produtos.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ProdutosService } from './produtos.service';
+import { ProdutosController } from './produtos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Produto } from './entities/produto.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Produto])],
+  controllers: [ProdutosController],
+  providers: [ProdutosService],
+})
+export class ProdutosModule {}
