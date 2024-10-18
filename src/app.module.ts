409cf5fa7fa6717b111d41569e0fb7dedcc39bd7
Luana Cardoso <luanacardoso.bc@gmail.com>
2024-10-18 01:45:32 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,27 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app/app.controller";
-import { AppService } from "./app/app.service";
-import { DatabaseModule } from "./database/database.module";
-import { ProdutosModule } from "./modules/produtos.module";
-import { AvaliacoesModule } from "./modules/avaliacoes.module";
+import { Module } from '@nestjs/common';
+import { AppController } from './app/app.controller';
+import { AppService } from './app/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Produto } from './entities/produto.entity'; 
+import { Avaliacao } from './entities/avaliacao.entity'; 
+import { ProdutoSubscriber } from './subscribers/produto-create.subscriber';
+import { AvaliacaoSubscriber } from './subscribers/avaliacao-create.subscriber';
+import { ProdutosModule } from './modules/produtos.module';
+import { AvaliacoesModule } from './modules/avaliacoes.module';
 
 @Module({
   imports: [
-    DatabaseModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', 
+      port: 5432, 
+      username: 'postgres', 
+      password: 'postgres', 
+      database: 'ecofeedbackhub', 
+      entities: [Produto, Avaliacao], 
+      subscribers: [ProdutoSubscriber, AvaliacaoSubscriber], 
+      synchronize: true, 
+    }),
     ProdutosModule,
     AvaliacoesModule,
   ],
@@ -15,3 +29,4 @@ import { AvaliacoesModule } from "./modules/avaliacoes.module";
   providers: [AppService],
 })
 export class AppModule {}
+
