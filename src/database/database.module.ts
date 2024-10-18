118c7cc4fa855ebb545510ef981e68ccd4c91873
Luana Cardoso <luanacardoso.bc@gmail.com>
2024-10-18 01:58:58 -0300

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,24 +1,34 @@
-iimport { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Produto } from './entities/produto.entity'; // exemplo de entidade
-import { Avaliacao } from './entities/avaliacao.entity'; // exemplo de entidade
-import { ProdutoSubscriber } from './subscribers/produto-create.subscriber';
-import { AvaliacaoSubscriber } from './subscribers/avaliacao-create.subscriber';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ProdutoSubscriber } from "./subscribers/produto-create.subscriber";
+import { AvaliacaoSubscriber } from "./subscribers/avaliacao-create.subscriber";
+import { Avaliacoes1729220182295 } from "./migrations/1729220182295-avaliacoes";
+import { Produtos1729220170057 } from "./migrations/1729220170057-produtos";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost', 
-      port: 5432, 
-      username: 'postgres', 
-      password: 'postgres', 
-      database: 'ecofeedbackhub', 
-      entities: [Produto, Avaliacao], 
-      subscribers: [ProdutoSubscriber, AvaliacaoSubscriber], 
-      synchronize: true, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: true, 
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: "postgres",
+        host: "localhost", 
+        port: 5432, 
+        database: "ecofeedbackhub", 
+        username: "postgres", 
+        password: "postgres", 
+        entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+        migrations: [Avaliacoes1729220182295, Produtos1729220170057],
+        autoLoadEntities: true,
+        subscribers: [ProdutoSubscriber, AvaliacaoSubscriber],
+        synchronize: false,
+      }),
     }),
   ],
 })
 export class DatabaseModule {}
-
